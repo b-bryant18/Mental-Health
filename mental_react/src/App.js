0ad08611393a80c48174b0b1cbb656ac19af5df8
Brad Bryant <bradleybryant86@gmail.com>
2020-04-22 00:56:03 -0500

    Created components folder. Made a folder for each component with js and css files inside

--- mental_react/src/App.js
+++ mental_react/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Navbar from "./components/Navbar;"
+import Header from "./components/Header;"
+import Wrapper from "./components/Wrapper;"
+import Home from "./pages/Home";
+import Workout from "./pages/Workout";
+import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Router>
+        <Navbar />
+        <Header />
+        <Wrapper>
+          <Route exact path ="/" component ={Home} />
+          <Route exact path = "/home" component = {Home} />
+          <Route exact path = "/workout" component = {Workout} />
+        </Wrapper>
+        <Footer />
+      </Router>
     </div>
   );
 }
